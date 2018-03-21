2ce50d49a70790c24a9d2d46abe2c178f8b6d2bb
Andrew Gibson <atgibson27@gmail.com>
2018-03-20 19:56:32 -0500

    load API_BASE_URL directly from environment variables

--- tools/config-js.sh
+++ tools/config-js.sh
@@ -11,6 +11,9 @@ source "${RYR_GLOBAL_CONFIG_DIR:="${HOME}/.config/ryr"}/ryr-env.sh"
 # Create the config file if needed.
 if [ ! -f "${CONFIG_JS}" ]; then
 cat << EOF > "${CONFIG_JS}"
-var config = {"GOOGLE_WEB_MAPS_API_KEY": "${RYR_WEB_GOOGLE_MAPS_API_KEY}"};
+var config = {
+  GOOGLE_WEB_MAPS_API_KEY: '${RYR_WEB_GOOGLE_MAPS_API_KEY}',
+  API_BASE_URL: '${API_BASE_URL}'
+};
 EOF
 fi
