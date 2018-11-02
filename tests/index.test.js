e76f90eccd9129b186d1be7b63203eb29a8c99e8
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-11-01 19:39:12 -0500

    Clean up the index page
    Removes the unused CSS selectors in the index page.
    
    Drive-by:
    * Prepare the main test to run in BDD style.

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,8 +4,8 @@ import renderer from 'react-test-renderer'
 
 import App from '../pages/index.js'
 
-describe('<Index />', () => {
-  it('renders <App /> components', () => {
+describe('Given we load our app', () => {
+  it('Then renders without crashing', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find(App)).toBeTruthy();
   });
