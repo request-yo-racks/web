3b8cab97cb2772a2aff9015f3aa2fe7aa39b8bd0
Toughee <MJ1menez@protonmail.com>
2018-12-02 20:30:50 -0600

    Update for instructions component - Added new css style for component - Made component look the same as ver 2.0 of web app - Added JSX to component

--- components/ryr-instructions.js
+++ components/ryr-instructions.js
@@ -1,42 +1,118 @@
 import PropTypes from 'prop-types';
 
-const stepsHeader = { textAlign: 'center', marginBottom: '0.8em', fontSize: '1.8em' };
-
-const stepsText = { textAlign: 'center', marginBottom: '1.5em' };
-const stepIndent = { textIndent: '-4.9em' };
-
-const circleStyle = {
-  borderRadius: '50%',
-  fontWeight: '970',
-  backgroundColor: '#06b700',
-  width: '100px',
-  height: '100px',
-  color: 'white',
-  padding: '3px 6px 3px 6px'
-};
-
-const NextStyle = {
-  marginBottom: '2em',
-  h3: '2em'
-};
-
 const RyrInstructions = props => {
-  let circleShape = props.size || circleStyle;
+
   return (
-    <div className={NextStyle}>
+    <div>
+      <div id="instructions" className="color-black">
+        <h2 className="center">What to do next?</h2>
+        <div className="instruction-list">
+          <ol className="list-numbered">
+            <li>
+              Send your email to <a href="mailto:bicycleprogram@austintexas.gov?subject=Bicycle Corral Request">
+                bicycleprogram@austintexas.gov
+              </a>
+            </li>
+            <li>
+                Set "Bicycle Corral Request" as the subject.
+            </li>
+          </ol>
+        </div>
+      </div>
+    <style jsx>
 
-      <h3 style={stepsHeader}>What to do next?</h3>
-
-      <p style={stepsText}>
-        <span style={circleShape}>1</span> Send your email to bicycleprogram@austintexas.gov.
-      </p>
+      {`
+      h2 {
+        text-align: center;
+        margin-bottom: 1em;
+      }
       
-      <p style={stepsText}>
-        <span style={circleShape}>2</span> Set &#8220;Bicycle Corral Request&#8221; as the subject.
-      </p>
-  
-    </div>
+      section {
+        padding: 15px;
+      }
 
+      #instructions {
+        background-color: #DDFDDF;
+      }
+
+      .circle {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 4.5em;
+        height: 4.5em;
+        border-radius: 50%;
+        background-color: white;
+        color: #5AAD54;
+      }  
+
+      .section-connector {
+        width: 100%;
+        height: 75px;
+        margin-bottom: -1em;
+        padding: 0;
+      }
+
+      .section-connector-location {
+        background-color: #2E5D2A;
+        fill: white;
+      }
+
+      .section-connector-preview {
+        background-color: #5AAD54;
+        fill: #2E5D2A;
+      }
+
+      .section-connector-instructions {
+        background-color: #DDFDDF;
+        fill: #5AAD54;
+      }
+
+      .instruction-list {
+        margin: auto;
+        max-width: 450px;
+      }
+
+      .instruction-list {
+        margin: auto;
+        width: 70%;
+
+      }
+
+      .list-numbered {
+        list-style: none;
+        margin-left: 1em;
+        counter-reset: line;
+      }
+
+      .list-numbered > li {
+        position: relative;
+        margin-bottom: 1.5em;
+      }
+
+      .list-numbered > li:before {
+        position: absolute;
+        left: -2.25em;
+        display: inline-block;
+        width: 1.25em;
+        height: 1.25em;
+        margin-right: 0.5em;
+        background-color: #129406;
+        border-radius: 50%;
+        color: #fff;
+        text-align: center;
+        vertical-align: middle;
+        line-height: 1.25em;
+        counter-increment: line;
+        content: counter(line);
+      }
+      .instruction-list {
+          width: 100%;
+        }
+      } 
+      `}
+    </style>
+    </div>
   );
 };
 
