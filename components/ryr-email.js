da2428638ceeaed20dc5f80fd4626efd58a603be
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-12-26 11:27:52 -0600

    Make the store a better place (#172)
    * Make devtools work with the Chrome/Firefox extension. This makes
      debugging so much easier.
    * Store more data in the store
      * marker location
      * place summary list
      * details of the selected place
    * Add types for future asynchronous actions.

--- components/ryr-email.js
+++ components/ryr-email.js
@@ -3,24 +3,32 @@ import { connect } from 'react-redux';
 import ReactMarkdown from 'react-markdown';
 
 const RyrEmail = props => {
-  const email = `
-  Thanks for contacting us on behalf of ${props.text}.
+  let email = '';
+  if (props.placeDetails != undefined && props.placeDetails.geometry != undefined) {
+    email = `
+  Thanks for contacting us on behalf of ${props.placeDetails.name}.
 
   Dear city of Austin,
 
-  I would like to request a bike rack at ${props.text}, geolocation  ${props.text}}. The business supporting this demand
-  is ${props.text}, located at ${props.text}.
+  I would like to request a bike rack at ${props.placeDetails.vicinity}, (geolocation:
+  ${props.placeDetails.geometry.location.lat}, ${
+      props.placeDetails.geometry.location.lng
+    }). The business supporting this demand
+  is ${props.placeDetails.name}.
 
-  The point of contact is ${props.text} and can be contacted via phone (${props.text}) or email (${props.text}).
+  The point of contact is ${props.placeDetails.name} and can be contacted via phone (${
+      props.placeDetails.name
+    }) or email (${props.placeDetails.name}).
 
-  Current parking situation: ${props.text}
+  Current parking situation: ${props.placeDetails.name}
 
-  ${props.text}
+  ${props.placeDetails.name}
 
   Cheers,
 
   Request Yo Racks Team
   `;
+  }
 
   return (
     <div
@@ -39,12 +47,12 @@ const RyrEmail = props => {
 };
 
 RyrEmail.propTypes = {
-  text: Proptypes.string
+  placeDetails: Proptypes.object
 };
 
 const mapStateToProps = state => {
-  const { text } = state;
-  return { text };
+  const { placeDetails } = state;
+  return { placeDetails };
 };
 
 export default connect(mapStateToProps)(RyrEmail);
